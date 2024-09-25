14b1bf51611aff7ed0891e36a0ba1fbd483cab10
NaterciaMM <naterciamm2020@gmail.com>
2024-09-25 14:42:23 -0300

    blaaaaaaaaablabla

---
+++ atividade22.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba vários números do usuário e some-os até que o número 0 seja digitado, momento em que o programa deve exibir o valor total.
+soma = 0
+
+while True:
+    num = int(input("Digite um numero ou 0 para parar "))
+    if num ==0:
+        break
+    soma += num
+
+
+print (f"{soma}")
